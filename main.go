789c44b43428df34fb7a2cc4639fb64dcab4243f
Norbert <wagner.norbert277@outlook.com>
2021-02-01 22:57:02 +0100

    Template option

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"net/smtp"
 	"os"
+	"strconv"
 	"strings"
+	"text/template"
 )
 
 func main() {
@@ -37,10 +40,34 @@ func main() {
 	smtpHost := "smtp.gmail.com"
 	smtpPort := "587"
 
-	fmt.Print("What message do you want to write? ")
-	e, _ := r.ReadString('\n')
-	messageS := strings.TrimSpace(e)
-	message := []byte(messageS)
+	fmt.Print("What type of message do you want to send: \n",
+		"	1. Typed, \n",
+		"	2. HTML template \n")
+	f, _ := r.ReadString('\n')
+	typeOfMessage := strings.TrimSpace(f)
+	typeOfMessageI, _ := strconv.Atoi(typeOfMessage)
+
+	var body bytes.Buffer
+	var message []byte
+
+	if typeOfMessageI == 1 {
+		fmt.Print("What message do you want to write? ")
+		e, _ := r.ReadString('\n')
+		messageS := strings.TrimSpace(e)
+		message = []byte(messageS)
+	} else if typeOfMessageI == 2 {
+		t, _ := template.ParseFiles("template.html")
+		fmt.Print("What's the subject of email? ")
+		subject, _ := r.ReadString('\n')
+		subject = strings.TrimSpace(subject)
+
+		mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+		body.Write([]byte(fmt.Sprintf("Subject: "+subject+"\n%s\n\n", mimeHeaders)))
+
+		t.Execute(&body, struct {
+		}{})
+		message = body.Bytes()
+	}
 
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
