83af1be314b7b10945fd10bfa6809346125d0695
Norbert <wagner.norbert277@outlook.com>
2021-02-01 23:04:52 +0100

    errorHandling(err) and readString() added

--- main.go
+++ main.go
@@ -13,25 +13,19 @@ import (
 
 func main() {
 	fmt.Print("Login: ")
-	r := bufio.NewReader(os.Stdin)
-	a, _ := r.ReadString('\n')
-	from := strings.TrimSpace(a)
+	from := readString()
 
 	fmt.Print("Password: ")
-	b, _ := r.ReadString('\n')
-	password := strings.TrimSpace(b)
+	password := readString()
 
 	to := []string{}
 
 	i := 0
 	for i != 1 {
 		fmt.Print("Who do you want to send an email to? ")
-		c, _ := r.ReadString('\n')
-		c = strings.TrimSpace(c)
-		to = append(to, c)
+		to = append(to, readString())
 		fmt.Print("Anyone else (yes/no)? ")
-		d, _ := r.ReadString('\n')
-		d = strings.TrimSpace(d)
+		d := readString()
 		if d != "yes" {
 			i = 1
 		}
@@ -43,23 +37,21 @@ func main() {
 	fmt.Print("What type of message do you want to send: \n",
 		"	1. Typed, \n",
 		"	2. HTML template \n")
-	f, _ := r.ReadString('\n')
-	typeOfMessage := strings.TrimSpace(f)
-	typeOfMessageI, _ := strconv.Atoi(typeOfMessage)
+	typeOfMessage := readString()
+	typeOfMessageI, err := strconv.Atoi(typeOfMessage)
+	errorHandling(err)
 
 	var body bytes.Buffer
 	var message []byte
 
 	if typeOfMessageI == 1 {
 		fmt.Print("What message do you want to write? ")
-		e, _ := r.ReadString('\n')
-		messageS := strings.TrimSpace(e)
+		messageS := readString()
 		message = []byte(messageS)
 	} else if typeOfMessageI == 2 {
 		t, _ := template.ParseFiles("template.html")
 		fmt.Print("What's the subject of email? ")
-		subject, _ := r.ReadString('\n')
-		subject = strings.TrimSpace(subject)
+		subject := readString()
 
 		mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
 		body.Write([]byte(fmt.Sprintf("Subject: "+subject+"\n%s\n\n", mimeHeaders)))
@@ -71,10 +63,21 @@ func main() {
 
 	auth := smtp.PlainAuth("", from, password, smtpHost)
 
-	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	errorHandling(err)
 	fmt.Println("Email Sent Successfully!")
+}
+
+func errorHandling(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readString() string {
+	r := bufio.NewReader(os.Stdin)
+	a, err := r.ReadString('\n')
+	errorHandling(err)
+	b := strings.TrimSpace(a)
+	return b
 }
